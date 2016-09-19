9181c50c393b5e909f31a442832521bd39f7740d
Trevor Eggenberger <trevore@utexas.edu>
2016-09-19 13:19:41 -0500

    seconds handler

--- Lab3/main.c
+++ Lab3/main.c
@@ -30,6 +30,8 @@ uint16_t COLORS[7] = {
 	ST7735_WHITE
 };
 
+uint32_t secondsElapsed = 0;
+
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 long StartCritical (void);    // previous I bit, disable interrupts
@@ -39,45 +41,17 @@ void WaitForInterrupt(void);  // low power mode
 void UserTask(void){
 }
 
-// This debug function initializes Timer0A to request interrupts
-// at a 100 Hz frequency.  It is similar to FreqMeasure.c.
-
-/*
-void Timer0A_Init100HzInt(void){
-  volatile uint32_t delay;
-  DisableInterrupts();
-  // **** general initialization ****
-  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
-  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
-  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
-  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
-  // **** timer0A initialization ****
-                                   // configure for periodic mode
-  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
-  TIMER0_TAILR_R = 799999*16;         // start value for 100 Hz interrupts
-  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
-  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
-  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
-  // **** interrupt initialization ****
-                                   // Timer0A=priority 2
-  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
-  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer0A timeout
+  secondsElapsed = (secondsElapsed+1)%86400;
 }
-*/
-/*void Timer0A_Handler(void){
-  TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
-  PF2 ^= 0x04;                   // profile
-}*/
 
 void init_lcd(void) {
   ST7735_InitR(INITR_REDTAB);				//LCD init
 }
 
 void init_timers(void){
-  //Timer1_Init(&UserTask, 181818); // initialize timer1 (slowest rate)
-	//Timer2_Init(&UserTask2, 7920); // initialize timer2 to ~99 us period
-
-  EnableInterrupts();
+	Timer0A_Init(&UserTask, 80000000);
 }
 void DelayWait10ms(uint32_t n){
 	uint32_t volatile time;
@@ -117,6 +91,7 @@ int main(void){
   init_switches();
 	init_lcd();
 	init_timers();
+	EnableInterrupts();
 	Start_Speaker();
  
 
--- Lab3/switch.c
+++ Lab3/switch.c
@@ -1,13 +1,15 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "../inc/tm4c123gh6pm.h"
 #include "PLL.h"
 #include "../PeriodicTimer0AInts_4C123/Timer0A.h"
 #include "switch.h"
 
 #define PD0 (*((volatile uint32_t *)0x40007004))
+#define PD1 (*((volatile uint32_t *)0x40007008))
 
-int count;
-volatile static unsigned long Last;      // previous
+int pd0_count, pd1_count, pb0_count, pf0_count, pf4_count;
+
 
 void SpeakerTask(void){
 	PB1 ^= 0x2;
@@ -55,56 +57,74 @@ void init_switches(void){
   GPIO_PORTB_AMSEL_R = 0;          // disable analog functionality on PF
 	GPIO_PORTB_PUR_R |= 0x1;				// pullup for PB0
 	
+	// PF0, PF4 are switch inputs
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
   GPIO_PORTF_DIR_R |= 0x6;        // make PF2, PF1 out (built-in LED)
-  GPIO_PORTF_AFSEL_R &= ~0x16;     // disable alt funct on PF2, PF1, PF4
-  GPIO_PORTF_DEN_R |= 0x16;        // enable digital I/O on PF2, PF1, PF4
+  GPIO_PORTF_AFSEL_R &= ~0x17;     // disable alt funct on PF2, PF1, PF4
+  GPIO_PORTF_DEN_R |= 0x17;        // enable digital I/O on PF2, PF1, PF4
                                    // configure PF2 as GPIO
-  GPIO_PORTF_PUR_R |= 0x10;         // pullup for PF4
+  GPIO_PORTF_PUR_R |= 0x11;         // pullup for PF0, PF4
   GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFF000F)+0x00000000;
   GPIO_PORTF_AMSEL_R = 0;          // disable analog functionality on PF
-	GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
-	GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
-  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
+	GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4 is edge-sensitive
+	GPIO_PORTF_ICR_R = 0x11;      // (e) clear flag4
+  //GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
   NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
-  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC  
+  //NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC  
 	
 		
-	// PD0 is switch input
-  GPIO_PORTD_AFSEL_R &= ~0x1;     // disable alt funct on PD0
-  GPIO_PORTD_DEN_R |= 0x1;        // enable digital I/O on PD0
+	// PD0, PD1 is switch input
+  GPIO_PORTD_AFSEL_R &= ~0x3;     // disable alt funct on PD0
+  GPIO_PORTD_DEN_R |= 0x3;        // enable digital I/O on PD0
   GPIO_PORTD_AMSEL_R = 0;          // disable analog functionality on PF
-	GPIO_PORTD_PUR_R |= 0x1;				// pullup for PD0
-	GPIO_PORTD_IS_R &= ~0x1;     // (d) PF4 is edge-sensitive
-	GPIO_PORTD_ICR_R = 0x1;      // (e) clear flag4
+	GPIO_PORTD_PUR_R |= 0x3;				// pullup for PD0
+	//GPIO_PORTD_IS_R &= ~0x1;     // (d) PF4 is edge-sensitive
+	//GPIO_PORTD_ICR_R = 0x1;      // (e) clear flag4
   //GPIO_PORTD_IM_R |= 0x1;      // (f) arm interrupt on PD0 *** No IME bit as mentioned in Book ***
-	NVIC_PRI0_R = (NVIC_PRI0_R&0x0FFFFFFF)|0xA0000000; // priority 5
+	//NVIC_PRI0_R = (NVIC_PRI0_R&0x0FFFFFFF)|0xA0000000; // priority 5
   //NVIC_EN0_R = 0x00000008;      // (h) enable interrupt 3 in NVIC  
 	
 	SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1   
 	Timer1Arm(50000);
 }
 
-
-static void GPIOArm(void){
-  GPIO_PORTD_ICR_R = 0x1;      // (e) clear flag4
-  GPIO_PORTD_IM_R |= 0x1;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
-}
-
 void GPIOPortD_Handler(void) {
 	GPIO_PORTD_IM_R &= ~0x1;     // disarm interrupt on PF4 
-	if (!Last) count++;
-	Timer1Arm(800000);
 }
 
 void GPIOPortF_Handler(void){
-	GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
-	count++;
+	if (!PF0) {
+		GPIO_PORTF_ICR_R = 0x1;      // (e) clear flag0
+		pf0_count++;
+	}
+	if (!PF4) {
+		GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
+		pf4_count++;
+	}
 }
 
 void Timer1A_Handler(void){
 	//TIMER1_ICR_R = 0x00000000;    // disarm timeout interrupt
 	TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
-  static uint16_t State = 0; // Current debounce status
-  State=(State<<1) | !PD0 | 0xe000;
-  if(State==0xf000)count++; 
+	
+  static uint16_t pd0State = 0; // Current debounce status
+  pd0State=(pd0State<<1) | !PD0 | 0xe000;
+  if(pd0State==0xf000)pd0_count++; 
+	
+	static uint16_t pf4State = 0; // Current debounce status
+  pf4State=(pf4State<<1) | !PF4 | 0xe000;
+	if(pf4State==0xf000)pf4_count++;
+	
+	static uint16_t pf0State = 0; // Current debounce status
+  pf0State=(pf0State<<1) | !PF0 | 0xe000;
+	if(pf0State==0xf000)pf0_count++;
+	
+	static uint16_t pb0State = 0; // Current debounce status
+  pb0State=(pb0State<<1) | !PB0 | 0xe000;
+	if(pb0State==0xf000)pb0_count++;
+	
+	static uint16_t pd1State = 0; // Current debounce status
+  pd1State=(pd1State<<1) | !PD1 | 0xe000;
+	if(pd1State==0xf000)pd1_count++;
 }
