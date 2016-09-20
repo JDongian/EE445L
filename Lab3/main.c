aa2bdc3dbb5d0c8127e77f1249035171fb3cd696
Joshua <jdong42@gmail.com>
2016-09-20 17:37:07 -0500

    refactor main

--- Lab3/main.c
+++ Lab3/main.c
@@ -19,6 +19,10 @@
 #include "drawLib/draw2D.h"
 #include "drawLib/math2.h"
 
+#define RADIUS SCREEN_WIDTH * 2 / 5 + 5
+#define CLOCK_CENTER point_new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 16)
+
+// Some predefined colors are convenient for rainbows
 uint16_t COLORS[7] = {
 	ST7735_BLUE,
 	ST7735_RED,
@@ -37,6 +41,7 @@ typedef struct timeStamp {
 } timeStamp;
 
 
+displayMode mode = ANALOG;
 uint32_t secondsElapsed = 0;
 timeStamp alarmTime;
 bool alarmOn = false;
@@ -61,28 +66,27 @@ void Init_LCD(void) {
 }
 
 void Init_Timers(void){
-	Timer0A_Init(&UserTask, 80000000);
-	alarmTime.hours = 0;
-	alarmTime.minutes = 0;
+  Timer0A_Init(&UserTask, 80000000);
+  alarmTime.hours = 0;
+  alarmTime.minutes = 0;
 }
 
 void drawHands(uint32_t time, point center, int radius, bool reset) {
-
-	int32_t degreesSeconds = (time % 60) * -6;
+  int32_t degreesSeconds = (time % 60) * -6;
   int32_t degreesMinutes = -1*(time % (60 * 60) / 10); 
   int32_t degreesHours = -1*((time % (60 * 60 * 12)) / 120);
 
-	// second hand
-	point sEnd = rotPoint(center, degreesSeconds, point_new(center.x, center.y + (radius - 4)));
-	drawLine(center, sEnd, reset? 0 : ST7735_RED);
+  // second hand
+  point sEnd = rotPoint(center, degreesSeconds, point_new(center.x, center.y + (radius - 4)));
+  drawLine(center, sEnd, reset ? 0 : ST7735_RED);
 	
   // minute hand
-	point mEnd = rotPoint(center, degreesMinutes, point_new(center.x, center.y + (radius - 10)));
-	drawLine(center, mEnd, reset? 0 : ST7735_BLUE);
-	
-	// hour hand
-	point hEnd = rotPoint(center, degreesHours, point_new(center.x, center.y + (radius/2)));
-	drawLine(center, hEnd, reset? 0 : ST7735_WHITE);
+  point mEnd = rotPoint(center, degreesMinutes, point_new(center.x, center.y + (radius - 10)));
+  drawLine(center, mEnd, reset ? 0 : ST7735_BLUE);
+  
+  // hour hand
+  point hEnd = rotPoint(center, degreesHours, point_new(center.x, center.y + (radius/2)));
+  drawLine(center, hEnd, reset ? 0 : ST7735_WHITE);
 }
 
 void drawHourHand(uint32_t time, point center, int radius, bool reset){
@@ -98,24 +102,53 @@ void drawMinuteHand(uint32_t time, point center, int radius, bool reset) {
 }
 
 void drawDigital(uint32_t time){
-	uint8_t hours = (time / 3600) % 24;
-	uint8_t minutes = (time  / 60) % 60;
-	uint8_t seconds = time % 60;
-	
-	ST7735_DrawCharS(18, 65, hours/10 + '0', ST7735_Color565(255, 0, 0), 0, 2);
+  uint8_t hours = (time / 3600) % 24;
+  uint8_t minutes = (time / 60) % 60;
+  uint8_t seconds = time % 60;
+
+  char buff[8] = {':'};
+  sprintf(buff, "%d%d:%d%d:%d%d",
+          hours/10, hours%10,
+          minutes/10, minutes%10,
+          seconds/10, seconds%10);
+  for (int i = 0; i < 8; ++i) {
+    ST7735_DrawCharS(18 + i * 12, 65, buff[i], ST7735_Color565(255, 0, 0), 0, 2);
+  }
+  /*
+  ST7735_DrawCharS(18, 65, hours/10 + '0', ST7735_Color565(255, 0, 0), 0, 2);
   ST7735_DrawCharS(30, 65, hours%10 + '0', ST7735_Color565(255, 0, 0), 0, 2);
   ST7735_DrawCharS(42, 65, ':', ST7735_Color565(255, 255, 255), 0, 2);
   ST7735_DrawCharS(54, 65, minutes/10 + '0', ST7735_Color565(0, 255, 0), 0, 2);
   ST7735_DrawCharS(66, 65, minutes%10 + '0', ST7735_Color565(0, 255, 0), 0, 2);
-	ST7735_DrawCharS(78, 65, ':', ST7735_Color565(255, 255, 255), 0, 2);
+  ST7735_DrawCharS(78, 65, ':', ST7735_Color565(255, 255, 255), 0, 2);
   ST7735_DrawCharS(90, 65, seconds/10 + '0', ST7735_Color565(0, 255, 255), 0, 2);
   ST7735_DrawCharS(102, 65, seconds%10 + '0', ST7735_Color565(0, 255, 255), 0, 2);
+  */
 }
 
+        
+void drawAnalog(uint32_t time, point center, uint16_t radius, uint16_t color) {
+        // Clear out previous hands
+        drawHands(time - 1, center, radius, true);
+        // Draw new second, minute, hour hands
+        drawHands(time, center, radius, false);
+       
+        // Redraw analog clock border
+        if (alarmOn) {
+          drawCircle(center, radius, color);
+          drawCircle(center, radius - 3, color);
+        } else {
+          // Display a green clock when alarm is not on.
+          drawCircle(center, radius, ST7735_GREEN);
+          drawCircle(center, radius - 3, ST7735_GREEN);
+        }
+}
+ 
+
 void drawString(int16_t x, int16_t y, char* s, int16_t textColor, int16_t bgColor, uint8_t textSize, uint16_t n){
-	for (int i = 0; i < n; i++){
-		ST7735_DrawCharS(6*i+x, y, s[i], textColor, bgColor, textSize);
-	}
+  for (int i = 0; i < n; i++){
+    ST7735_DrawCharS(6*i+x, y, s[i], textColor, bgColor, textSize);
+  }
 }
 
 void displayAlarmStatus(bool setAlarm) {
@@ -155,94 +188,87 @@ void displayAlarmStatus(bool setAlarm) {
 	
 }
 
-bool timeEquals(uint32_t t1, uint32_t t2) {
-	return t1 == t2;
+bool timeEquals(timeStamp t1, uint32_t t2) {
+    return t1.hours * 3600 + t1.minutes * 60 == t2
 }
 
+void handleButtons() {
+  if (alarmOn) {
+      Start_Speaker();
+  } else {
+      Stop_Speaker();
+  }
+
+  if (alarmOnPressed){
+      if (alarmOn) {
+          alarmOn = false;
+      }
+      alarmSet ^= 1;
+      alarmOnPressed = false;
+  }
+
+  if (changeModePressed) {
+      changeModePressed = false;
+      mode ^= 1;
+      ST7735_FillScreen(0);
+  }
+
+  if (setAlarmPressed){
+      setAlarmPressed = false;
+      settingAlarm ^= 1;
+  }
+
+  if (hourChangePressed) {
+      if (settingAlarm){
+          alarmTime.hours = (alarmTime.hours + 1) % 24;
+      }
+      else {
+          drawHourHand(secondsElapsed, center, radius, true);
+          secondsElapsed += 3600;
+      }
+      hourChangePressed = false;
+  }
+
+  if (minuteChangePressed) {
+      if (settingAlarm){
+          alarmTime.minutes = (alarmTime.minutes + 1) % 60;
+      }
+      else {
+          drawHourHand(secondsElapsed, center, radius, true);
+          drawMinuteHand(secondsElapsed, center, radius, true);
+          secondsElapsed += 60;
+      }
+      minuteChangePressed = false;
+  }
+}
+
+
 int main(void){
-	int color = 0;
-	bool settingAlarm = true;
+  int color = 0;
+  bool settingAlarm = true;
   Init_Switches();
   Init_LCD();
   Init_Timers();
-  displayMode mode = ANALOG;
 	  
-  static int radius = SCREEN_WIDTH * 2 / 5 + 5;
-  point center = point_new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 16);
-
   while(1) {
-		if (alarmOn) {
-			Start_Speaker();
-		}
-		else Stop_Speaker();
-		
-		if (alarmOnPressed){
-			if (alarmOn) alarmOn = false;
-			alarmSet ^= 1;
-			alarmOnPressed = false;
-		}
-		
-		if (changeModePressed) {
-			changeModePressed = false;
-			mode ^= 1;
-			ST7735_FillScreen(0);
-		}
-		
-		if (setAlarmPressed){
-			setAlarmPressed = false;
-			settingAlarm ^= 1;
-		}
-		
-		color = (color+1) % 105;
-		
+    color = (color+1) % 105;
+
+    handleButtons();
+    // Trigger alarm when the daily alarm time has come
+    if (timeEquals(alarmTime, secondsElapsed) && alarmSet){
+        alarmOn = true;
+    }
+
     switch (mode) {
       case ANALOG:
-        // Clear out previous hands
-        drawHands(secondsElapsed - 1, center, radius, true);
-        // Draw new second, minute, hour hands
-        drawHands(secondsElapsed, center, radius, false);
-       
-        // Redraw analog clock border
-				if (alarmOn) {
-					drawCircle(center, radius, COLORS[color/15]);
-					drawCircle(center, radius - 3, COLORS[color/15]);
-				}
-				else {
-					drawCircle(center, radius, ST7735_GREEN);
-					drawCircle(center, radius - 3, ST7735_GREEN);
-				}
+        drawAnalog(secondsElapsed, CLOCK_CENTER, RADIUS, COLORS[color/15]);
         break;
       case DIGITAL:
-					drawDigital(secondsElapsed);
-          break;
-      default:
-          break;
-    }
-		if (hourChangePressed) {
-			if (settingAlarm){
-				alarmTime.hours = (alarmTime.hours + 1) % 24;
-			}
-			else {
-				drawHourHand(secondsElapsed, center, radius, true);
-				secondsElapsed += 3600;
-			}
-			hourChangePressed = false;
-		}
-		if (minuteChangePressed) {
-			if (settingAlarm){
-				alarmTime.minutes = (alarmTime.minutes + 1) % 60;
-			}
-			else {
-				drawHourHand(secondsElapsed, center, radius, true);
-				drawMinuteHand(secondsElapsed, center, radius, true);
-				secondsElapsed += 60;
-			}
-			minuteChangePressed = false;
-		}
-		if (timeEquals(alarmTime.hours*3600 + alarmTime.minutes*60, secondsElapsed) && alarmSet){
-			alarmOn = true;
-		}
-		displayAlarmStatus(settingAlarm);
+      case default:
+        drawDigital(secondsElapsed);
+        break;
+    } 
+    displayAlarmStatus(settingAlarm);
   }
 }
 
