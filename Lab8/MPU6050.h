740b562cc048df4953625e63d3aeefb5dad8d9eb
Trevor Eggenberger <trevore@utexas.edu>
2016-10-27 17:23:51 -0500

    esp and mpu headers

---
+++ Lab8/MPU6050.h
@@ -0,0 +1,14 @@
+/* MPU6050.h
+ * Trevor Eggenberger - tae498
+ * Joshua Dong - jid295
+ */
+
+#ifndef __MPU6050__
+#define __MPU6050__
+
+
+void MPU6050_Init(void);
+uint16_t MPU6050_ReadAccel(char axis);
+uint16_t MPU6050_ReadGyro(char axis);
+
+#endif /* __MPU6050__ */
